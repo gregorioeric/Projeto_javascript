bd25beac6df39e3219391a73e2581b48c28d4ed2
gregorioeric <gregorio.eric@gmail.com>
2025-08-28 14:27:29 -0300

    Criamos funções no javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,29 @@ const modalRegister = document.querySelector("#modal-register");
 const btnCancelLogin = document.querySelector("#btn-cancel-login");
 const btnCancelRegister = document.querySelector("#btn-cancel-register")
 
+function soma(num01, num02) {
+  const somar = num01 + num02;
+  console.log(somar);
+}
+
+soma(8, 6)
+
+// arrow function 
+const multiplicar = (num01, num02) => {
+  return num01 * num02
+}
+
+console.log(multiplicar(5, 8.9));
+
+
+
+
+
+
+
+// addEventListener => Escuta o evento de click
+// e executa a função
+// eventos => click, focus, change, input, keypress, keyup, keydown
 btnLogin.addEventListener("click", (e) => {
   modalLogin.classList.remove("hide-modal");
   modalLogin.classList.add("show-modal");
