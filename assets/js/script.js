16edf2abe8fb37bf7c3af3cc5dc9557329799923
gregorioeric <gregorio.eric@gmail.com>
2025-09-04 13:28:52 -0300

    Atualizado o javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,22 +3,22 @@ const btnRegister = document.querySelector("#btn-register");
 const modalLogin = document.querySelector("#modal-login");
 const modalRegister = document.querySelector("#modal-register");
 const btnCancelLogin = document.querySelector("#btn-cancel-login");
-const btnCancelRegister = document.querySelector("#btn-cancel-register")
+const btnCancelRegister = document.querySelector("#btn-cancel-register");
 const btnLoginOpen = document.querySelector("#btn-login-open");
 const inputEmail = document.querySelector("#email");
-const resultado = document.querySelector("#resultado")
+const resultado = document.querySelector("#resultado");
 
 function soma(num01, num02) {
   const somar = num01 + num02;
   console.log(somar);
 }
 
-soma(5, 8)
+soma(5, 8);
 
-// arrow function 
+// arrow function
 const multiplicar = (num01, num02) => {
-  return num01 * num02
-}
+  return num01 * num02;
+};
 
 console.log(multiplicar(5, 8.9));
 
@@ -28,27 +28,35 @@ console.log(multiplicar(5, 8.9));
 btnLogin.addEventListener("click", (e) => {
   modalLogin.classList.remove("hide-modal");
   modalLogin.classList.add("show-modal");
-})
+});
 
 btnRegister.addEventListener("click", (e) => {
   modalRegister.classList.remove("hide-modal");
   modalRegister.classList.add("show-modal");
-})
+});
 
 btnCancelLogin.addEventListener("click", (e) => {
+  e.preventDefault();
   modalLogin.classList.remove("show-modal");
   modalLogin.classList.add("hide-modal");
-})
+});
 
 btnCancelRegister.addEventListener("click", (e) => {
+  e.preventDefault();
   modalRegister.classList.remove("show-modal");
   modalRegister.classList.add("hide-modal");
-})
+});
 
 // inputEmail.addEventListener("input", () => {
 //   resultado.innerHTML = inputEmail.value
 // })
 
 inputEmail.addEventListener("focus", () => {
-  inputEmail.style.borderBottom = "0.5rem solid rgb(255, 0, 0)"
-})
+  inputEmail.style.borderBottom = "0.5rem solid rgb(255, 0, 0)";
+});
+
+btnLoginOpen.addEventListener("click", (e) => {
+  e.preventDefault();
+  const email = inputEmail.value;
+  resultado.innerHTML = `<h1>${email}</h1>`;
+});
